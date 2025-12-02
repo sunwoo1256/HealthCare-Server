c55067295a625a7f87efaecbf14c6ea8eb948f98
sunwoo <sswsjw1234@gmail.com>
2025-12-02 14:00:12 +0900

    refactor: delete try-catch in UserAuthController (#12)

--- src/main/java/cerberus/HealthCare/global/exception/GlobalExceptionHandler.java
+++ src/main/java/cerberus/HealthCare/global/exception/GlobalExceptionHandler.java
@@ -1,9 +1,12 @@
 package cerberus.HealthCare.global.exception;
 
 import cerberus.HealthCare.global.common.BaseResponse;
+import io.jsonwebtoken.ExpiredJwtException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -47,6 +50,25 @@ public class GlobalExceptionHandler {
 
         return BaseResponse.of(HttpStatus.BAD_REQUEST, errorMessage);
     }
+
+    /**
+     * Spring Security 예외
+     */
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<BaseResponse<?>> handleBadCredentials() {
+        return BaseResponse.of(HttpStatus.UNAUTHORIZED, "이메일 또는 비밀번호가 일치하지 않습니다.");
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<BaseResponse<?>> handleAccessDenied() {
+        return BaseResponse.of(HttpStatus.FORBIDDEN, "접근 권한이 없습니다.");
+    }
+
+    @ExceptionHandler(ExpiredJwtException.class)
+    public ResponseEntity<BaseResponse<?>> handleExpiredJwt() {
+        return BaseResponse.of(HttpStatus.UNAUTHORIZED, "로그인이 만료되었습니다. 다시 로그인해주세요.");
+    }
+
 
     /**
      * 그 외 알 수 없는 예외
